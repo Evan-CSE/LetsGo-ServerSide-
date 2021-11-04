20f675648d8327589cf10723699145757e0cc7a7
= <=>
2021-11-04 17:19:12 -0400

    All done by the grace of almighty

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { ObjectID } = require('bson');
 const { object } = require('webidl-conversions');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
